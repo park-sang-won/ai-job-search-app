d90bebadd0cfaee17d98967b56e3be406282cce6
hussarian <kimjk2213@gmail.com>
2024-11-29 17:56:15 +0900

    백엔드입니다

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBo_jKQ4Ihsv7q5AnuTUbj7jEw7OnQihM",
+  authDomain: "jobaba-4ca97.firebaseapp.com",
+  projectId: "jobaba-4ca97",
+  storageBucket: "jobaba-4ca97.firebasestorage.app",
+  messagingSenderId: "67124286362",
+  appId: "1:67124286362:web:4b07382e519d12edd6a023"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
