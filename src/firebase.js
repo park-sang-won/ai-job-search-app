38c43d830ca9d699c3145b7e5c5c3abd1928c034
hussarian <98077075+hussarian@users.noreply.github.com>
2024-12-02 15:44:15 +0900

    인덱스 파일 업데이트입니다

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { firebase, firestore } from "firebase/app";
 import { getAuth } from 'firebase/auth'; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,6 +8,7 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: "AIzaSyBBo_jKQ4Ihsv7q5AnuTUbj7jEw7OnQihM",
   authDomain: "jobaba-4ca97.firebaseapp.com",
+  databaseURL: "https://jobaba-4ca97-default-rtdb.firebaseio.com",
   projectId: "jobaba-4ca97",
   storageBucket: "jobaba-4ca97.firebasestorage.app",
   messagingSenderId: "67124286362",
@@ -16,5 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+const firestore = firebase.firestore();
 
 export { app, auth };
